df8bbc6886c911378803f1c21b21a7e30fa7c878
Qazi Fahim Farhan <farhan@sharetrip.net>
2024-12-29 09:45:29 +0600

    :zap: Add maven-publish plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,6 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(project(":styleabletoast"))
+//    implementation("com.github.sharetripnet:StyleableToast:v0.0.4")
+
 }
